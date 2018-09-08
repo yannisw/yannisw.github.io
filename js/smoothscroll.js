7cf36882fd474dfb8678da5f297dd690de2e5e3c
yannisw <yannisw2010@gmail.com>
2018-09-08 10:14:17 +1000

    added smooth scroll

---
+++ js/smoothscroll.js
@@ -0,0 +1,30 @@
+$(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+   // store hash
+   var hash = this.hash;
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50 
+     }, 700, function(){
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});
+
+$("a.navbar-brand").on('click', function(e) {
+   // prevent default anchor click behavior
+   e.preventDefault();
+   // store hash
+   var hash = this.hash;
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top -50 
+     }, 700, function(){
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});
+
